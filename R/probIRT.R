39f60e0e4c531078e1c8a8fd3a6623cfa0689477
Waldir Leoncio <waldir.leoncio@gmail.com>
2017-02-15 16:50:27 +0100

    Stole probIRT from likEquate (needed by simExam).

---
+++ R/probIRT.R
@@ -0,0 +1,16 @@
+#' Probability of Getting an Item Correct
+#'
+#' @description Calculates the probability of getting an item correct under a 2-parameter IRT
+#' model.
+#'
+#' @param theta examinee skill
+#' @param a     item discimination
+#' @param b     item difficulty
+#'
+#' @return Probability of getting a correct item.
+#' @export
+probIRT <- function(theta, a, b) {
+  ex   <- exp(a * (theta - b))
+  prob <- ex / (1 + ex)
+  return(prob)
+}
