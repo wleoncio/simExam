d38eadf94076fe4980fe94f1527316cd6288311a
Waldir Leoncio <waldir.leoncio@gmail.com>
2018-06-15 16:30:17 +0200

    Added function to generate true ability parameters

---
+++ R/genAbilityParms.R
@@ -0,0 +1,17 @@
+#' Generate True Ability Parameters
+#'
+#' @param forms.n number of forms
+#' @param mu.first mean ability of first form
+#' @param sd.first standard deviation of ability of first form
+#' @param mu.last mean ability of last form
+#' @param sd.last standard deviation of ability of last form
+#'
+#' @return List containing means and standard deviations per form
+#' @export
+#'
+#' @examples
+genAbilityParms <- function(forms.n, mu.first, sd.first,
+                            mu.last = mu.first, sd.last = sd.first) {
+  list(mu = seq(mu.first, mu.last, length = forms.n),
+       sd = seq(sd.first, sd.last, length = forms.n))
+}
